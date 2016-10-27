d8abc4152642dbfe278ab0d0fc4b319fb8bc853c
ElainePaiz <linan.elaine@gmail.com>
2016-10-27 13:04:47 -0500

    Add files via upload

---
+++ Day3ConditionalStatement.cs
@@ -0,0 +1,22 @@
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution {
+
+    static void Main(String[] args) {
+        int N = Convert.ToInt32(Console.ReadLine());
+  //above are there originally
+  if (N%2==1) {
+                     Console.WriteLine("Weird");
+                    }
+        else {
+              if (N>=2 && N <= 5) {Console.WriteLine("Not Weird");}
+              else{ if (N <=20) {Console.WriteLine("Weird");}
+                    else {Console.WriteLine("Not Weird");}
+                  };
+
+             };
+    }
+}
