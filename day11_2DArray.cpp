091e235f62ca451ab08e326f6a211c7df3c93e13
ElainePaiz <linan.elaine@gmail.com>
2016-10-30 15:28:44 -0500

    Create day11_2DArray.cpp

---
+++ day11_2DArray.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    vector< vector<int> > arr(6,vector<int>(6));
+    for(int arr_i = 0;arr_i < 6;arr_i++){
+       for(int arr_j = 0;arr_j < 6;arr_j++){
+          cin >> arr[arr_i][arr_j];
+       }
+    }
+    
+    int max = -9*7;
+    for (int i = 1; i <= 4; i++) {
+        for ( int j = 1; j<=4; j++) {
+            int sum = 0;
+            sum = arr[i][j] + arr[i-1][j] + arr[i+1][j] + arr[i-1][j-1] + arr[i-1][j+1] + arr[i+1][j-1] + arr[i+1][j+1];
+            if (sum >max) {max = sum;}    
+        }
+            
+    }
+    cout<<max<<endl;
+        
+    return 0;
+}
